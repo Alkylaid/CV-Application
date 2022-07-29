ed10b016adca1e601745e0a0877db7cd971cb4d8
Alkylaid <alkylaid@gmail.com>
2022-07-29 00:09:58 -0500

    Updates

---
+++ precache-manifest.01ff180a6787cbfdfa4a3579ded63944.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b3e249bb7403b07f406",
+    "url": "/CV-Application/static/css/main.e5b1d2d9.chunk.css"
+  },
+  {
+    "revision": "4b3e249bb7403b07f406",
+    "url": "/CV-Application/static/js/main.8b3800e1.chunk.js"
+  },
+  {
+    "revision": "75598e577a8cdef59892",
+    "url": "/CV-Application/static/js/runtime~main.89d40007.js"
+  },
+  {
+    "revision": "8c30b3c9bb13f730a01d",
+    "url": "/CV-Application/static/js/2.8fb5fd9c.chunk.js"
+  },
+  {
+    "revision": "fe2ed3b2343ea3fbdb25c30725c2ba1c",
+    "url": "/CV-Application/index.html"
+  }
+];
